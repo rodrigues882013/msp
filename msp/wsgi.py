167c9bbf9ef1bc65baf95b16664a2bf9e4e138e1
Felipe <rodrigues882007@gmail.com>
2016-09-24 09:06:44 -0300

    Initial commit

---
+++ msp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for msp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "msp.settings")
+
+application = get_wsgi_application()
